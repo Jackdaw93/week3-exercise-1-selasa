9fab03a643d30ef7bc1a9593e6caddec28a07a74
Rohmad <rohmad37@ymail.com>
2020-06-23 13:51:58 +0700

    Update Exercise 2 Senin

--- script.js
+++ script.js
@@ -34,3 +34,40 @@ async function addFilm() {
     console.error(error);
   }
 }
+
+let position = document.querySelector(".show");
+let url1 = `https://5ef168da1faf160016b4d5c4.mockapi.io/api/film`;
+let options1 = {
+  method: "GET",
+  headers: {
+    "Content-type": "application/json",
+  },
+};
+
+fetch(url1, options1)
+  .then((response) => response.json())
+  .then((results) => {
+    for (let data in results) {
+      console.log(results);
+
+      let card = document.createElement("div");
+      card.innerHTML = `
+      <div class="card rounded" style="width: 18rem;">
+      <img src="${results[data].imgUrl}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title text-center">${results[data].title}</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+    <li class="list-group-item">Genre: ${results[data].genre}</li>
+    <li class="list-group-item">Release Year: ${results[data].release}</li>
+    <li class="list-group-item">Synopsis: ${results[data].descript}</li>
+    <li class="list-group-item">
+    <button class="btn btn-primary" id='btn-edit'>Edit</button>
+    <button class="btn btn-danger" id='bnt-del'>Delete</button>
+    </li>
+      </ul>
+    </div>
+    `;
+      position.appendChild(card);
+    }
+  });
